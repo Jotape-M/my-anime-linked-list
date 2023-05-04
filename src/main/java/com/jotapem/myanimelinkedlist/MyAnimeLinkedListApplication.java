c4356916ea5f413e92763e1256148a561f891156
Jotape-M <joaopedro.m2507@gmail.com>
2023-05-04 18:43:03 -0300

    first commit

---
+++ src/main/java/com/jotapem/myanimelinkedlist/MyAnimeLinkedListApplication.java
@@ -0,0 +1,13 @@
+package com.jotapem.myanimelinkedlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimeLinkedListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnimeLinkedListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jotapem/myanimelinkedlist/MyAnimeLinkedListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotapem.myanimelinkedlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnimeLinkedListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
